7a4ed763450e45c99141d8656ac5bcbccae89e80
Jean Thomas <jeniferjeanthomas@gmail.com>
2020-10-29 14:08:11 -0400

    Initial commit

---
+++ LibraryTerminal/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryTerminal
+{
+    abstract class Item
+    {
+        public string Title { get; set; }
+        public ItemStatus ItemStatus { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public Item() { }
+        public Item(string Title, ItemStatus ItemStatus, DateTime DueDate)
+        {
+            this.Title = Title;
+            this.ItemStatus = ItemStatus;
+            this.DueDate = DueDate;
+        }
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Status: {ItemStatus}");
+            if (ItemStatus.Equals("CheckedOut"))
+            {
+                Console.WriteLine($"Return Date: {DueDate}");
+            }
+            else
+            {
+                Console.WriteLine($"Status: {ItemStatus}");
+            }
+        }
+    }
+}
---
+++ LibraryTerminal/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LibraryTerminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool userContinue = true;
+            Console.WriteLine("Welcome to the Library!");
+            while (userContinue)
+            {
+                LibraryMenu();
+                string input = GetUserInput($"What would you like to do?");
+            }
+        }
+        public static string GetUserInput(string message)
+        {
+            Console.WriteLine(message);
+            string input = Console.ReadLine().Trim();
+            return input;
+        }
+        public static void LibraryMenu()
+        {
+            Console.WriteLine($"1. Display items.");
+            Console.WriteLine($"2. Search for a book by author.");
+            Console.WriteLine($"3. Search for a book by title.");
+            Console.WriteLine($"4. Show items you have checked out.");
+            Console.WriteLine($"5. Return a book.");
+            Console.WriteLine($"6. Quit.");
+        }
+        public static bool UserContinue(string message)
+        {
+            Console.WriteLine(message);
+            string proceed = Console.ReadLine().Trim().ToLower();
+
+            while (proceed != "n" && proceed != "y")
+            {
+                Console.WriteLine("Invalid input. Please enter 'y' to continue or 'n' to exit.");
+                proceed = Console.ReadLine().Trim().ToLower();
+            }
+
+            if (proceed == "y")
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
