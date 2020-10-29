d519df9f20de925d69daef4ea190cce10d075821
L. Jensen <02Jensenl@github.com>
2020-10-29 15:49:04 -0400

    Laurens commit

--- LibraryTerminal/Item.cs
+++ LibraryTerminal/Item.cs
@@ -6,22 +6,24 @@ namespace LibraryTerminal
 {
     abstract class Item
     {
+        
         public string Title { get; set; }
         public ItemStatus ItemStatus { get; set; }
         public DateTime DueDate { get; set; }
-
+        public int ReleaseYear { get; set; }
         public Item() { }
-        public Item(string Title)
+        public Item(string Title, int ReleaseYear)
         {
             this.Title = Title;
             this.ItemStatus = ItemStatus.OnShelf;
             this.DueDate = DateTime.Now;
+            this.ReleaseYear = ReleaseYear;
         }
-
         public virtual void PrintInfo()
         {
             Console.WriteLine($"Title: {Title}");
             Console.WriteLine($"Status: {ItemStatus}");
+            Console.WriteLine($"Year Released: {ReleaseYear}");
             if (ItemStatus.Equals("CheckedOut"))
             {
                 Console.WriteLine($"Return Date: {DueDate}");
