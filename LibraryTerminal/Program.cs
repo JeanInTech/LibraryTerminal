85854845016141ecd2ce110bf280af4dfdd98700
Bart Martinon <barthelemy.martinon@gmail.com>
2020-10-30 14:10:24 -0400

    Implemented Input half of FileI/O, new Library constructor, SavedItems.txt to serve as the source of Items

--- LibraryTerminal/Program.cs
+++ LibraryTerminal/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Security.Cryptography.X509Certificates;
 
 namespace LibraryTerminal
@@ -7,8 +9,20 @@ namespace LibraryTerminal
     {
         static void Main(string[] args)
         {
+            List<Item> itemsLoaded = new List<Item>();            
+            StreamReader reader = new StreamReader("../../../SavedItems.txt");
+            string line = reader.ReadLine();
+            while (line != null)
+            {
+                string[] itemEntryInfo = line.Split("|"); // Record line
+                Item newItem = GenerateItem(itemEntryInfo);
+                itemsLoaded.Add(newItem);
+                line = reader.ReadLine();
+            }
+            reader.Close();
 
-            Library L = new Library();
+            //Library L = new Library();
+            Library L = new Library(itemsLoaded);
             L.PrintItems();
 
             bool userContinue = true;
@@ -51,6 +65,28 @@ namespace LibraryTerminal
             }
             else
                 return false;
+        }
+
+        public static Item GenerateItem(string[] itemInfo)
+        {
+            string itemType = itemInfo[0].ToLower();
+            if (itemType.Equals("book"))
+            {
+                return new Book(itemInfo[1], itemInfo[2],int.Parse(itemInfo[3]), int.Parse(itemInfo[4]));
+            }
+            else if (itemType.Equals("cd"))
+            {
+                return new CD(itemInfo[1], itemInfo[2], int.Parse(itemInfo[3]), itemInfo[4]);
+            }
+            else if (itemType.Equals("dvd"))
+            {
+                return new DVD(itemInfo[1], itemInfo[2], int.Parse(itemInfo[3]), int.Parse(itemInfo[4]));
+            }
+            else if (itemType.Equals("magazine"))
+            {
+                return new Magazine(itemInfo[1], itemInfo[2], int.Parse(itemInfo[3]), int.Parse(itemInfo[4]));
+            }
+            return null;
         }
     }
 }
